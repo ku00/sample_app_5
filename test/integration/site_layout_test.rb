bf0b24fbf383ac3e67a16eee869c386e909cfe4a
ku00 <skentaro36@gmail.com>
2018-02-11 17:41:31 +0900

    [5.3.4_*] 演習: ヘルパーメソッドのテストを用意

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,4 +10,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test "full title helper" do
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
 end
