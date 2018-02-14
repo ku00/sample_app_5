5bc39fd33c0e60bc9d42933768118f287e0d6b56
ku00 <skentaro36@gmail.com>
2018-02-15 00:02:58 +0900

    [6.2.4_*] 演習2: フォーマットの正規表現をより厳しく

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     presence: true,
     length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
     presence: true,
     length: { maximum: 255 },
