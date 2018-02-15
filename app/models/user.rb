f254905402a6f3ed13823cd9f460c6618fd47cfa
ku00 <skentaro36@gmail.com>
2018-02-15 22:45:37 +0900

    [6.2.5_*] 一意性を検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   validates :email,
     presence: true,
     length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  before_save { self.email = email.downcase }
 end
