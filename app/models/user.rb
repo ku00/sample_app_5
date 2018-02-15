bded7a8efc1448d2838d86301392190afa8ddfd8
ku00 <skentaro36@gmail.com>
2018-02-15 22:48:51 +0900

    [6.2.5_*] 演習: メールアドレスを小文字にするテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 end
