78ce678277c07b1a54765212befd9f3dfe2ea6f0
ku00 <skentaro36@gmail.com>
2018-02-15 23:05:09 +0900

    [6.3.3] パスワードの最小文字数をバリデーションで追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ApplicationRecord
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
-  before_save { email.downcase! }
-
   has_secure_password
+  validates :password,
+    presence: true,
+    length: { minimum: 6 }
+
+  before_save { email.downcase! }
 end
