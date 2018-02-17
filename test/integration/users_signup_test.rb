afda865fcf046eb55d5d33d1bdbe6a7926838ebb
ku00 <skentaro36@gmail.com>
2018-02-17 15:01:44 +0900

    [7.4.4] ユーザー登録成功時のテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "valid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.blank?
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action="/signup"]'
