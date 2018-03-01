95217ce847f93c9c63583dfe9ffe47062f7e1fcf
ku00 <skentaro36@gmail.com>
2018-03-01 23:11:36 +0900

    :bug: partial化によって新規登録のformにsignup_pathを指定することができなくなっていた
    ref: https://github.com/ku00/sample_apps_5/pull/8/commits/eec4703638c29274536a40ab427a5f18b29a5f8c

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
-    assert_select 'form[action="/signup"]'
     assert_difference 'User.count', 1 do
       post signup_path, params: {
         user: {
@@ -22,7 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
-    assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
       post signup_path, params: {
         user: {
