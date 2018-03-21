55ba45bdc84c0f95ae0d45d88f1cf17517df718b
ku00 <skentaro36@gmail.com>
2018-03-21 18:38:30 +0900

    users#destroyをAPI化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:destroy]
+  before_action :logged_in_user, only: []
   before_action :correct_user, only: []
-  before_action :admin_user, only: [:destroy]
+  before_action :admin_user, only: []
 
   def index
     @users = User.paginate(page: params[:page])
@@ -35,8 +35,6 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
   end
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create user" do
-    assert_difference 'User.count' do
+    assert_difference('User.count') do
       post users_url, params: {
         user: {
           name: 'lawrence',
@@ -56,18 +56,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.reload.admin?
   end
 
-  test "should redirect destory when not logged in" do
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user), as: :json
     end
-    assert_redirected_to login_url
-  end
 
-  test "should redirect destroy when logged in as a non-admin" do
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
-    end
-    assert_redirected_to root_url
+    assert_response 204
   end
 end
