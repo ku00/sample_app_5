d8760c2ef332088821372291cd413cde315b06f7
ku00 <skentaro36@gmail.com>
2018-03-25 23:47:28 +0900

    RSAを文字列で渡してもJWTは利用できない
    OpenSSL::PKey::RSAのインスタンスを渡す必要がある

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,3 +1,5 @@
+require 'openssl'
+
 class JsonWebToken
   ALGORITHM = 'RS256'
 
@@ -5,11 +7,22 @@ class JsonWebToken
     def encode(payload, expire = 1.days)
       payload[:expire] = expire.to_i
 
-      JWT.encode(payload, ENV['JWT_PRIVATE_KEY'], ALGORITHM)
+      JWT.encode(payload, rsa_private, ALGORITHM)
     end
 
     def decode(token)
-      body = JWT.decode(token, ENV['JWT_PUBLIC_KEY'], true, { algorithm: ALGORITHM })
+      JWT.decode(
+        token,
+        rsa_private.public_key,
+        true,
+        { algorithm: ALGORITHM }
+      )
     end
+
+    private
+
+      def rsa_private
+        OpenSSL::PKey::RSA.new(ENV['JWT_PRIVATE_KEY'])
+      end
   end
 end
