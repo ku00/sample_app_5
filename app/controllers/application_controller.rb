73827f0c65dac76160fd357a507bb2b55b276ec7
ku00 <skentaro36@gmail.com>
2018-04-01 18:09:12 +0900

    リクエストのJWTをデコードできるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   include SessionsHelper
+
+  def authenticate_user
+    unless request_token
+      render json: { errors: 'Invalid request' }, status: :unauthorized and return
+    end
+
+    JsonWebToken.decode(request_token)
+  end
+
+  def request_token
+    return unless auth_header = request.headers['Authorization']
+
+    scheme, token = auth_header.split(' ')
+    scheme == 'Bearer' ? token : nil
+  end
 end
